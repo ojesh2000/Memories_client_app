14ec59db49ca44658c7175a3ffdacf3754aed6d3
ojesh2000 <102738908+ojesh2000@users.noreply.github.com>
2022-08-23 20:19:39 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -19,17 +19,3 @@ import axios from 'axios';
 
  export const signIn = (formData) => API.post("/user/signin" , formData);
  export const signUp = (formData) => API.post("/user/signup" , formData);
-
-//  import axios from 'axios';
-
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
-
-
-// export const fetchPosts = () => API.get('/posts');
-// export const createPosts = (newPost) => API.post('/posts', newPost);
-// export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-// export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-// export const deletePost = (id) => API.delete(`/posts/${id}`);
-
-// export const signIn = (formData) => API.post('/user/signin', formData);
-// export const signUp = (formData) => API.post('/user/signup', formData);
